69772b71732409e9e876f07fa0f43580052b774a
shinyrainbow <kornkanok.lieng@gmail.com>
2026-01-30 15:54:58 +0700

    d

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -66,7 +66,7 @@ export default async function HomePage() {
       >
         {/* Background Image */}
         <img
-          src="/home-banner.PNG"
+          src="/home-banner.png"
           alt="Packpert Banner"
           className="absolute inset-0 w-full h-full object-cover"
         />
